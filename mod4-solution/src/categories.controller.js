c12ac4f96ea881ed204c6f24a9452b87febf0742
alfha101 <ahamard@gmail.com>
2016-10-12 16:55:51 +0200

    Module 4 Assignment

---
+++ mod4-solution/src/categories.controller.js
@@ -0,0 +1,16 @@
+(function () {
+'use strict';
+
+angular.module('Data')
+.controller('CategoriesListController', CategoriesListController);
+
+
+CategoriesListController.$inject = ['items'];
+function CategoriesListController(items) {
+  var list =this;
+
+  list.items = items.data;
+
+};
+
+})();
---
+++ mod4-solution/src/items.component.js
@@ -0,0 +1,13 @@
+
+(function () {
+'use strict';
+
+angular.module('Data')
+.component('itemsList', {
+  templateUrl: 'src/templates/items-list.template.html',
+  bindings: {
+    items: '<'
+  }
+});
+
+})();
